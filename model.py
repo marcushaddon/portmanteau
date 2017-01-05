ed4aaa2fd802308a0067f836f5b7b15bd3cc46fc
marcushaddon <haddon.marcus@gmail.com>
2017-01-05 16:19:55 -0600

    [TASK] Add first/last syllables/phones @properties to Word class

--- model.py
+++ model.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script     import Manager
 from flask_migrate    import Migrate, MigrateCommand
 from flask            import request
+# wrapper around hyphenate_word so everything doesnt break if we rewrite that later
+from hyphenate        import hyphenate_word as word_to_syllables
+import pronouncing
 import json
 
 app = Flask(__name__)
@@ -20,6 +23,35 @@ class Word(db.Model):
     definition      = db.Column(db.String(2400))
     leading_phones  = db.Column(db.String(20))
     trailing_phones = db.Column(db.String(20))
+
+    @property
+    def syllables(self):
+        return word_to_syllables(self.word)
+
+    @property
+    def leading_syllable(self):
+        return self.syllables[0]
+
+    @property
+    def trailing_syllable(self):
+        return self.syllables[-1]
+
+    @property
+    def first_phone(self):
+        if self.leading_phones:
+            return self.leading_phones
+        else:
+            phones = pronouncing.phones_for_word(self.leading_syllable)
+        return phones
+
+    @property
+    def last_phone(self):
+        if self.trailing_phones:
+            phones = self.trailing_phones
+        else:
+            # To be replaced with method to actually get it
+            phones = pronouncing.phones_for_word(self.trailing_syllable)
+        return phones
 
     def __init__(self, word = "", wordtype = "", definition = ""):
         self.word       = word
